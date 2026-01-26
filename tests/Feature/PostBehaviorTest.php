10cf9427c37b20a7232c9cb712a2f890ef3a52b3
Abubakr <110747776+abubakerKhaled@users.noreply.github.com>
2026-01-26 11:35:07 +0200

    feat: Implement post reply functionality and add corresponding feature tests.

--- tests/Feature/PostBehaviorTest.php
+++ tests/Feature/PostBehaviorTest.php
@@ -18,3 +18,24 @@ test('it allows a profile to publish a post', function(){
     expect($post->parent_id)->toBeNull();
     expect($post->repost_of_id)->toBeNull();
 });
+
+test('can reply to post', function(){
+    $orginal = Post::factory()->create();
+
+    $replier = Profile::factory()->create();
+    $reply = Post::reply($replier, $orginal, 'This is a reply');
+
+    expect($reply->parent->is($orginal))->toBeTrue();
+    expect($orginal->replies)->toHaveCount(1);
+});
+
+
+test('can have many replies', function(){
+    $orginal = Post::factory()->create();
+
+    $replies = Post::factory()->count(4)->reply($orginal)->create();
+
+    expect($replies->first()->parent->is($orginal))->toBeTrue();
+    expect($orginal->replies)->toHaveCount(4);
+    expect($orginal->replies->contains($replies->first()))->toBeTrue();
+});
